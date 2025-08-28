13c57a24ae940369acec98057607e42a7ea8edbd
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 22:48:54 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'DADDY YANKEE';
+export let nameSongs = [
+  'LA NUEVA Y LA EX' /*1*/,
+  'LIMBO' /*2*/,
+  'LO QUE PASÓ, PASÓ' /*3*/,
+  'CON CALMA' /*4*/,
+  'POSE' /*5*/,
+  'LLAMADO DE EMERGENCIA' /*6*/,
+  'QUE TENGO QUE HACER' /*7*/,
+  'DURA' /*8*/,
+  'ELLA ME LEVANTÓ' /*9*/,
+  'LA DESPEDIDA' /*10*/,
+  'LOVUMBA' /*11*/,
+  'GASOLINA' /*12*/,
+  'DESPACITO (CON LUIS FONSI)' /*13*/,
+  'SÍGUEME Y TE SIGO' /*14*/,
+  'VEN CONMIGO' /*15*/,
+  'MOVIENDO CADERAS' /*16*/,
+  'PASARELA' /*17*/,
+  'SHAKY SHAKY' /*18*/,
+  'LA DESPEDIDA - V2' /*19*/,
+  'LA SEÑAL' /*20*/
+];
